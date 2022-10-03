57f7b2f25559560533cb65742d5cdbf7001852d9
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-10-02 23:06:49 -0700

    updated models

--- server/models/driver.js
+++ server/models/driver.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const driverSchema = mongoose.Schema({
   firstname: String,
   lastname: String,
+  startdate: Date,
 });
 
 const driver = mongoose.model("driver", driverSchema);
--- server/models/expense.js
+++ server/models/expense.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const expenseSchema = mongoose.Schema({
   type: String,
-  cost: Number,
+  date: Date,
+  name: String,
+  location: String,
+  price: Number,
+  employee: String,
 });
 
 const expense = mongoose.model("expense", expenseSchema);
