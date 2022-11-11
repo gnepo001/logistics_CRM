77c39a45e01eb09adc1f6f070057dad77205dcc2
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-11-10 16:40:12 -0800

    fetch type logic added for different types of expenses

--- server/controllers/expenses.js
+++ server/controllers/expenses.js
@@ -3,7 +3,7 @@ import expense from "../models/expense.js";
 //fetchdata
 export const getExpenses = async (req, res) => {
   try {
-    const postMessages = await expense.find();
+    const postMessages = await expense.find().sort({ date: 1 });
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -22,11 +22,20 @@ export const createExpense = async (req, res) => {
 };
 
 export const getExpenseByType = async (req, res) => {
-  const type = req.query.Fuel;
-  try {
-    const data = await expense.find({ type: "Fuel" });
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
+  const type = req.query;
+  if (type.type == "all") {
+    try {
+      const data = await expense.find();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(409).json({ message: error.message });
+    }
+  } else {
+    try {
+      const data = await expense.find({ type: "Fuel" });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(409).json({ message: error.message });
+    }
   }
 };
