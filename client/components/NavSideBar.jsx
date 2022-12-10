d08ed8eb620050b597890146c5a49cee583e5e8c
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-12-09 18:37:31 -0800

    closed menu UI edit

--- client/components/NavSideBar.jsx
+++ client/components/NavSideBar.jsx
@@ -38,14 +38,14 @@ const NavSideBar = () => {
           className="flex flex-col md:w-56 h-screen border-black border-r-2"
           style={{ backgroundColor: "var(--cerulean)" }}
         >
-          <div className="py-5 border-grey-400 border-b-2 mx-5 mt-4 flex flex-row relative">
+          <div className="py-4 border-grey-400 border-b-2 mx-2 mt-3 w-[90%] flex flex-row relative justify-center ">
             <button
               className="text-4xl text-white "
               onClick={() => setOpen(!open)}
             >
               <RxCross1 className="border-zinc-200 border-2 rounded-xl" />
             </button>
-            <div className="mx-5">Company Name</div>
+            <div className="mx-5 mt-3">Company Name</div>
           </div>
           <NavButton tag="DashBoard" icon={<AiOutlineDashboard />} link={"/"} />
           <NavButton tag="Clients" icon={<BsPeople />} link={"/clients"} />
@@ -66,9 +66,10 @@ const NavSideBar = () => {
           className="flex flex-col w-20 h-screen border-black border-r-2 "
           style={{ backgroundColor: "var(--cerulean)" }}
         >
-          <button onClick={() => setOpen(!open)}>
-            <GiHamburgerMenu className="text-4xl text-white mx-auto mt-10" />
+          <button onClick={() => setOpen(!open)} className="mt-0">
+            <GiHamburgerMenu className="text-4xl text-white mx-auto mt-9 " />
           </button>
+          <hr className="text-grey-400 mt-2 w-[85%] mx-auto" />
           <MiniNavButton
             tag="DashBoard"
             icon={<AiOutlineDashboard />}
